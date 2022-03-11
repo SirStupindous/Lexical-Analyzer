e4c87ebf47fa2f7a703563e1e2dd62163ed1a965
nickayson <aysonnicholas@gmail.com>
2022-03-10 19:01:38 -0800

    Co-authored-by: SirStupindous <SirStupindous@users.noreply.github.com>

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <fstream>
+
+void Lexer()
+{
+    // file pointer
+    fstream fout;
+  
+    // opens an existing csv file or creates a new file.
+    fout.open("reportcard.csv", ios::out | ios::app);
+    for(int i = 0;i < n; n++) {
+        char = getChar();
+        if (char == "\" && )
+    }
+}
